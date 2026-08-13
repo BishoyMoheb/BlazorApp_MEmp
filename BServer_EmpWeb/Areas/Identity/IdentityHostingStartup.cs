33249b288c5087a5dd7a40c1d754b2a7ded09db2
BishoyMoheb <75308234+BishoyMoheb@users.noreply.github.com>
2026-08-13 17:13:00 +0300

    Add files via upload

---
+++ BServer_EmpWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BServer_EmpWeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BServer_EmpWeb.Areas.Identity.IdentityHostingStartup))]
+namespace BServer_EmpWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BServer_EmpWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BServer_EmpWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BServer_EmpWebContext>();
+            });
+        }
+    }
+}
